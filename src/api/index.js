0ad5514e466d09f7e646d32faf06bd28bae82976
Rhitik_HBR <ritikbhandari17@gmail.com>
2024-04-04 15:14:04 +0545

    changed

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const publicApi = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://parking-api-otv3.onrender.com",
 });
 
 const privateApi = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://parking-api-otv3.onrender.com",
 });
 
 privateApi.interceptors.request.use((config) => {
